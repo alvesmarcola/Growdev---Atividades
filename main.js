514b5fb163602b9487a9aaea3b2bef62c431c8ec
alvesmarcolas <alvesbrunom8@gmail.com>
2024-03-03 04:47:32 -0300

    Atividade 1 PHP e atualizando HUB

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var cells = document.querySelectorAll('.card')
+  const cells = document.querySelectorAll('.card')
 
   cells.forEach(function(cell) {
     cell.querySelector('.js-expander').addEventListener('click', function() {
-      var thisCell = this.closest('.card')
+      const thisCell = this.closest('.card')
 
       if (!thisCell.classList.contains('is-expanded')) {
         cells.forEach(function(otherCell) {
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     cell.querySelector('.js-collapser').addEventListener('click', function(event) {
       event.stopPropagation();
-      var thisCell = this.closest('.card');
+      const thisCell = this.closest('.card');
 
       thisCell.classList.remove('is-expanded');
       thisCell.classList.add('is-collapsed');
